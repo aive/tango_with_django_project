0207f6e1c3ffba458b66cc757be33a724a17d6fa
Aive Kalmus <2288799k@student.gla.ac.uk>
2018-02-12 21:08:02 +0000

    Ch6 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -15,29 +15,28 @@ def populate():
 
      python_pages = [
          {"title": "Official Python Tutorial",
-         "url":"http://docs.python.org/2/tutorial/"},
+         "url":"http://docs.python.org/2/tutorial/", "views": 30},
          {"title":"How to Think like a Computer Scientist",
-         "url":"http://www.greenteapress.com/thinkpython/"},
+         "url":"http://www.greenteapress.com/thinkpython/", "views": 15},
          {"title":"Learn Python in 10 Minutes",
-         "url":"http://www.korokithakis.net/tutorials/python/"} ]
+         "url":"http://www.korokithakis.net/tutorials/python/", "views": 12} ]
 
      django_pages = [
          {"title":"Official Django Tutorial",
-         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 10},
          {"title":"Django Rocks",
-         "url":"http://www.djangorocks.com/"},
-         {"title":"How to Tango with Django",
-         "url":"http://www.tangowithdjango.com/"} ]
+         "url":"http://www.djangorocks.com/", "views": 8},
+         {"title":"How to Tango with Django", "url":"http://www.tangowithdjango.com/", "views": 4} ]
 
      other_pages = [
          {"title":"Bottle",
-         "url":"http://bottlepy.org/docs/dev/"},
+         "url":"http://bottlepy.org/docs/dev/", "views": 2},
          {"title":"Flask",
-         "url":"http://flask.pocoo.org"} ]
+         "url":"http://flask.pocoo.org", "views": 45} ]
 
-     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
-         "Django": {"pages": django_pages, "views": 64, "likes": 32},
-         "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
+     cats = {"Python": {"pages": python_pages, "views": 129, "likes": 65},
+         "Django": {"pages": django_pages, "views": 65, "likes": 34},
+         "Other Frameworks": {"pages": other_pages, "views": 33, "likes": 17} }
 
 
      # If you want to add more catergories or pages,
