24f7db4be7ad8cfe33f78f9ebfec4267be60fbba
Aive Kalmus <2288799k@student.gla.ac.uk>
2018-02-12 19:15:55 +0000

    Ch.6

--- rango/urls.py
+++ rango/urls.py
@@ -4,18 +4,19 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'about/$', views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
 ]
 
-# the previous command is appending a call to the static() function
-# to your project’s urlpatterns list.
+    # the previous command is appending a call to the static() function
+    # to your project’s urlpatterns list.
 
-# also added 2 import commands at top, for settings and static
-# Once this is complete, you should be able to serve content from
-# the media directory of your project from the /media/ URL.
+    # also added 2 import commands at top, for settings and static
+    # Once this is complete, you should be able to serve content from
+    # the media directory of your project from the /media/ URL.
 
+    # MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
-# MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-
-# MEDIA_ROOT = MEDIA_DIR
-# MEDIA_URL = '/media/'
+    # MEDIA_ROOT = MEDIA_DIR
+    # MEDIA_URL = '/media/'
