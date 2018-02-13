2da1d91bc2ca3f0afbf15bb3990861e484778952
Aive Kalmus <2288799k@student.gla.ac.uk>
2018-02-13 15:40:57 +0000

    Ch 9 end

--- rango/views.py
+++ rango/views.py
@@ -66,6 +66,7 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context_dict)
 
 #aive added for ch 8
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -91,6 +92,7 @@ def add_category(request):
         # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -206,7 +208,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    #return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html',{})
 
 # Use the login_required() decorator to ensure only those logged in can
 # access the view.
